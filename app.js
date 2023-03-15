c76faac7cf64542ee4dae976c7dfd422cb773c47
Pranay-Kumar-1215 <kumarpranay1215@gmail.com>
2023-03-15 19:32:30 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('So, the first aws project is completed!'));
+app.get('/', (req, res) => res.send('So, the first aws project is completed and the pipeline deployment is also complete'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
